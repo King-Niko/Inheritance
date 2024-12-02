a72ddc2fe3d8514c94013fd46cb821f03d52cabf
King-Niko <nickd_walling!@yahoo.com>
2024-12-02 16:51:49 -0500

    Finished the driver for Inheritance project.

--- Inheritance/Inheritance.cpp
+++ Inheritance/Inheritance.cpp
@@ -11,6 +11,47 @@ using namespace std;
 
 int main()
 {
-	cout << "hello";
+	// Declare variables for user input
+	string manufacturer;
+	int yearBuilt, numDoors;
+	double towingCapacity;
+
+	// Test the vehicle class
+	cout << "Enter the manufacturer: ";
+	getline(cin, manufacturer);
+	cout << "Enter the year built: ";
+	cin >> yearBuilt;
+	cin.ignore(); // To ignore newline left by previous input
+
+	Vehicle vehicle(manufacturer, yearBuilt);
+	cout << "\nVehicle Info:" << endl;
+	vehicle.displayInfo();
+
+	// Test the car class
+	cout << "\nEnter the manufacturer: ";
+	getline(cin, manufacturer);
+	cout << "Enter the year built: ";
+	cin >> yearBuilt;
+	cout << "Enter number of doors: ";
+	cin >> numDoors;
+	cin.ignore();
+
+	Car car(manufacturer, yearBuilt, numDoors);
+	cout << "\nCar Info:" << endl;
+	car.displayInfo();
+
+	// Test the truck class
+	cout << "\nEnter the manufacturer: ";
+	getline(cin, manufacturer);
+	cout << "Enter the year built: ";
+	cin >> yearBuilt;
+	cout << "Enter towing capcity (in lbs): ";
+	cin >> towingCapacity;
+	cin.ignore();
+
+	Truck truck(manufacturer, yearBuilt, towingCapacity);
+	cout << "\nTruck Info:" << endl;
+	truck.displayInfo();
+
 	return 0;
 }
